af32bd2b74a0c0296d78f54a856dcd2c725f0605
PE51K <grigorij1m@gmail.com>
2024-03-25 19:16:43 +0300

    Реализация обработки видео

--- bot/handlers/new_transcription.py
+++ bot/handlers/new_transcription.py
@@ -1,3 +1,5 @@
+from moviepy.editor import VideoFileClip
+
 from telegram import (
     Update,
     File,
@@ -15,7 +17,7 @@ from database import add_transcription_to_db
 from inline_keyboard_markups import create_main_menu_markup, create_cancel_adding_new_transcription_markup
 
 AWAITING_NEW_TRANSCRIPTION_TITLE = 1
-AWAITING_NEW_TRANSCRIPTION_AUDIO = 2
+AWAITING_NEW_TRANSCRIPTION_AUDIO_OR_VIDEO = 2
 
 
 # noinspection PyUnusedLocal
@@ -50,13 +52,13 @@ async def handle_new_transcription_title(
         .reply_text(
             text=(
                 f"Отлично! "
-                "Теперь отправьте аудиозапись для транскрибации:"
+                "Теперь отправьте аудио и видеозапись для транскрибации:"
             ),
             reply_markup=create_cancel_adding_new_transcription_markup()
         )
     )
 
-    return AWAITING_NEW_TRANSCRIPTION_AUDIO
+    return AWAITING_NEW_TRANSCRIPTION_AUDIO_OR_VIDEO
 
 
 async def handle_new_transcription_audio(
@@ -70,6 +72,55 @@ async def handle_new_transcription_audio(
 
     new_transcription_audio_file: File = await context.bot.get_file(new_transription_audio_file_id)
     new_transcription_audio_file_path: str = new_transcription_audio_file.file_path
+
+    add_transcription_to_db(
+        chat_id=current_chat_id,
+        title=new_transcription_title,
+        audio_file_path=new_transcription_audio_file_path,
+        status="Awaiting upload to transcription API"
+    )
+
+    await (
+        update
+        .message
+        .reply_text(
+            text=(
+                f"Ваша транскрипция '{new_transcription_title}' добавлена в очередь обработки. "
+                "Чем я могу ещё помочь?"
+            ),
+            reply_markup=create_main_menu_markup()
+        )
+    )
+
+    context.user_data.pop('new_transcription_title', None)
+
+    return ConversationHandler.END
+
+
+def extract_audio(video_path: str, audio_path: str) -> None:
+    with VideoFileClip(video_path) as video:
+        video.audio.write_audiofile(audio_path, )
+
+
+async def handle_new_transcription_video(
+        update: Update,
+        context: ContextTypes.DEFAULT_TYPE
+) -> int:
+    new_transription_video_file_id: str = update.message.video.file_id
+    current_chat_id: int = update.effective_chat.id
+
+    new_transcription_title: str = context.user_data.get('new_transcription_title', 'Без названия')
+
+    new_transcription_video_file: File = await context.bot.get_file(new_transription_video_file_id)
+    new_transcription_video_file_path: str = new_transcription_video_file.file_path
+
+    new_transcription_audio_file_path = "/".join(
+        new_transcription_video_file_path.split("/")[:-2]
+        +
+        ["music", f"{new_transription_video_file_id}.mp3"]
+    )
+
+    extract_audio(new_transcription_video_file_path, new_transcription_audio_file_path)
 
     add_transcription_to_db(
         chat_id=current_chat_id,
@@ -134,10 +185,14 @@ handle_add_new_transcription = ConversationHandler(
                 handle_new_transcription_title
             )
         ],
-        AWAITING_NEW_TRANSCRIPTION_AUDIO: [
+        AWAITING_NEW_TRANSCRIPTION_AUDIO_OR_VIDEO: [
             MessageHandler(
                 filters.AUDIO,
                 handle_new_transcription_audio
+            ),
+            MessageHandler(
+                filters.VIDEO,
+                handle_new_transcription_video
             )
         ]
     },
