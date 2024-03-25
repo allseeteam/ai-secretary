f4e1f7a7773c483357635b49bb6f2079bd502c64
PE51K <grigorij1m@gmail.com>
2024-03-25 15:49:02 +0300

    Интеграция ChatGPT в обсуждение транскрипций

--- bot/handlers/discuss_transcription.py
+++ bot/handlers/discuss_transcription.py
@@ -1,3 +1,8 @@
+import asyncio
+
+from openai import OpenAI
+from pydantic_settings import BaseSettings
+
 from telegram import (
     Update,
     CallbackQuery
@@ -9,12 +14,23 @@ from telegram.ext import (
     ContextTypes,
     CallbackQueryHandler
 )
-
 from inline_keyboard_markups import (
     create_transcription_menu_markup_without_callback,
     create_stop_transcription_discussion_markup
 )
+from database import get_transcription_text_by_id
 
+
+class OpenAISettings(BaseSettings):
+    openai_api_key: str
+
+    class Config:
+        env_file = 'env/.env.openai'
+
+
+settings = OpenAISettings()
+
+openai_client = OpenAI(api_key=settings.openai_api_key)
 
 DISCUSSING_TRANSCRIPTION = 1
 
@@ -24,10 +40,23 @@ async def start_transcrition_discussion(
         update: Update,
         context: ContextTypes.DEFAULT_TYPE
 ) -> int:
+    global openai_client
+
     callback_query: CallbackQuery = update.callback_query
     transcription_id: str = callback_query.data.split(":")[-1]
 
     context.user_data['transcription_id'] = transcription_id
+
+    transcripton_text: str = get_transcription_text_by_id(transcription_id)
+    context.user_data['transcripton_assistant'] = openai_client.beta.assistants.create(
+        name=f"Transcription Assistant {transcription_id}",
+        instructions=(
+            "На основе данной текстовой транскрипции переговоров нескольких человек, отвечай на все мои вопросы:\n"
+            f"```\n{transcripton_text}\n```"
+        ),
+        model="gpt-4-turbo-preview",
+    )
+    context.user_data['transcription_thread'] = openai_client.beta.threads.create()
 
     await callback_query.answer()
 
@@ -46,21 +75,38 @@ async def handle_transcription_discussion_user_message(
         update: Update,
         context: ContextTypes.DEFAULT_TYPE
 ) -> int:
+    global openai_client
+
     message_text: str = update.message.text
 
-    if 'message_count' not in context.user_data:
-        context.user_data['message_count'] = 0
-    context.user_data['message_count'] += 1
+    openai_client.beta.threads.messages.create(
+        thread_id=context.user_data['transcription_thread'].id,
+        role="user",
+        content=message_text
+    )
+
+    run = openai_client.beta.threads.runs.create(
+        thread_id=context.user_data['transcription_thread'].id,
+        assistant_id=context.user_data['transcripton_assistant'].id,
+    )
+
+    while run.status in ['queued', 'in_progress', 'cancelling']:
+        await asyncio.sleep(1)
+        run = openai_client.beta.threads.runs.retrieve(
+            thread_id=context.user_data['transcription_thread'].id,
+            run_id=run.id
+        )
+
+    assistant_responce_text = (
+        openai_client.beta.threads.messages.list(thread_id=context.user_data['transcription_thread'].id)
+        .dict()['data'][0]['content'][0]['text']['value']
+    )
 
     await (
         update
         .message
         .reply_text(
-            text=(
-                f"{message_text}\n\n"
-                f"Всего сообщений: {context.user_data['message_count']}"
-                "Мой ответ: ..."
-            ),
+            text=assistant_responce_text,
             reply_markup=create_stop_transcription_discussion_markup()
         )
     )
@@ -73,6 +119,8 @@ async def stop_transcription_discussion(
         update: Update,
         context: ContextTypes.DEFAULT_TYPE
 ) -> int:
+    global openai_client
+
     callback_query: CallbackQuery = update.callback_query
     await callback_query.answer()
 
@@ -81,6 +129,10 @@ async def stop_transcription_discussion(
 
     context.user_data.pop('message_count', None)
     context.user_data.pop('transcription_id', None)
+
+    openai_client.beta.assistants.delete(
+        assistant_id=context.user_data['transcripton_assistant'].id
+    )
 
     await context.bot.send_message(
         chat_id=chat_id,
